ce51f013b3addfa47bf11eeb608f442cf364b859
Emmanuel <mono_devok1111@protonmail.com>
2019-01-15 20:37:10 -0500

    Start-> Goes into low power mode (screen goes dim and even off if possible)

--- app/src/main/java/develop/com/glitterbomb/MainActivity.java
+++ app/src/main/java/develop/com/glitterbomb/MainActivity.java
@@ -2,6 +2,8 @@ package develop.com.glitterbomb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mOnClick(View v) {
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.flags |= WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+        params.screenBrightness = 0;
+        getWindow().setAttributes(params);
     }
 }
